c3d8a0c950eea2edd7632550cf49be20e0c44172
Rabin <rabin@ingits.com>
2024-02-22 23:18:15 +0545

    requirements added

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # US-Visa-Approval-Prediction
 
+### Create virtual environment
+```
+conda create -p venv python=3.8 -y
+```
+
+### Activate virtual environment
+#### In windows
+```
+conda activate venv/
+```
+#### In Linux
+```
+source activate ./venv
+
+### Install requirements
+```
+pip install -r requirements.txt
+```
+
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools 
+
+setuptools.setup(
+    name="us_visa",
+    version="1.0.0",
+    author="rabin",
+    author_email="rabinsth1111@gmail.com",
+    description="US Visa Approval Prediction",
+    #long_description=open("README.md", "r").read(),
+    #long_description_content_type="text/markdown",
+    #url="https://github.com/joseph-wong/US-Visa-Approval-Prediction",
+    packages=setuptools.find_packages(),
+    # classifiers=[
+    #     "Programming Language :: Python :: 3",
+    #     "License :: OSI Approved :: MIT License",
+    #     "Operating System :: OS Independent",
+    # ],
+
+)
